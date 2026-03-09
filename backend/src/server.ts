01645e1ab3e4cea041fd1f56e0c356cb828eff60
Tien Phi <phivu7747@gmail.com>
2026-03-09 11:24:33 +0700

    Change port to 80

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -105,8 +105,8 @@ app.delete("/customers/:id", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(80, () => {
+    console.log("Server running on http://localhost:80");
 });
 
 
